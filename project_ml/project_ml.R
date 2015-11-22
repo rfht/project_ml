d0c5c32cf413bb2bad3f161734bde4b1d33308e5
thfrwn <tfrohwein@gmail.com>
2015-11-22 14:15:32 -0500

    Initial commit

---
+++ project_ml/project_ml.R
@@ -0,0 +1,2 @@
+# R script to create a prediction model for the manner in which the exercise was done (variable 'classe')
+download.file(url="https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv",destfile="pml-training.csv")
